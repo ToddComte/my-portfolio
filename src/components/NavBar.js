6ad2eca9d334456003b2e9e0ec442adfa3b6c49d
ToddComte <comtetodd@gmail.com>
2021-10-12 13:28:08 -0600

    add docker

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,12 +6,32 @@ export default function NavBar() {
     <header className="bg-blue-400">
       <div className="container mx-auto flex justify-between">
         <nav className="flex">
-          <NavLink to="/" exact>
+          <NavLink
+            to="/"
+            exact
+            activeClassName="text-white"
+            className="inflex-flex items-center py-6 px-3 mr-4 text-blue-900 hover:text-red-600 text-4xl font-blod cursive tracking-widest"
+          >
             Todd Comte
           </NavLink>
-          <NavLink to="/post">Blog Post</NavLink>
-          <NavLink to="/project">Projects</NavLink>
-          <NavLink to="/about">About Me!</NavLink>
+          <NavLink
+            to="/post"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-blue-900 hover:text-red-600"
+          >
+            Blog Post
+          </NavLink>
+          <NavLink
+            to="/project"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-blue-900 hover:text-red-600"
+          >
+            Projects
+          </NavLink>
+          <NavLink
+            to="/about"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-blue-900 hover:text-red-600"
+          >
+            About Me!
+          </NavLink>
         </nav>
       </div>
     </header>
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// const colors = require("tailwindcss/colors");
+
+// module.exports = {
+//   mode: "jit",
+//   purge: ["./src/**/*.[html,js,svelte,ts}"],
+//   theme: {
+//     extend: {},
+//     colors: colors,
+//   },
+//   plugins: [],
+// };
