b36ca8fb8f13357b52f89817fc1f716a28a5a438
ToddComte <comtetodd@gmail.com>
2021-11-01 11:07:48 -0600

    fixed the look of portfoilo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,14 +3,14 @@ import { NavLink } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <header className="bg-blue-400">
+    <header className="bg-blue-600">
       <div className="container mx-auto flex justify-between">
         <nav className="flex">
           <NavLink
             to="/"
             exact
             activeClassName="text-white"
-            className="inflex-flex items-center py-6 px-3 mr-4 text-gray-400 hover:text-gray-100 text-4xl font-blod cursive tracking-widest"
+            className="inflex-flex items-center py-6 px-3 mr-4 text-gray-400 hover:text-gray-100 text-4xl font-blod tracking-widest"
           >
             Todd Comte
           </NavLink>
