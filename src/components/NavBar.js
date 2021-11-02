33716a4dcf7a78ff85942d03148b5e2c4cbe7d90
ToddComte <comtetodd@gmail.com>
2021-11-02 10:40:19 -0600

    made a few more changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,36 +3,40 @@ import { NavLink } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <header className="bg-blue-400">
+    <header className="bg-blue-800">
       <div className="container mx-auto flex justify-between">
         <nav className="flex">
           <NavLink
             to="/"
             exact
             activeClassName="text-white"
-            className="inflex-flex items-center py-6 px-3 mr-4 text-gray-400 hover:text-gray-100 text-4xl font-blod cursive tracking-widest"
+            className="inflex-flex items-center py-6 px-3 mr-4 text-gray-400 hover:text-gray-100 text-4xl font-blod tracking-widest"
           >
             Todd Comte
           </NavLink>
           <NavLink
             to="/post"
+            activeClassName="text-red-100 bg-red-700"
             className="inline-flex items-center py-3 px-3 my-6 rounded text-gray-400 hover:text-gray-100"
           >
             Blog Post
           </NavLink>
           <NavLink
             to="/project"
+            activeClassName="text-red-100 bg-red-700"
             className="inline-flex items-center py-3 px-3 my-6 rounded text-gray-400 hover:text-gray-100"
           >
             Projects
           </NavLink>
           <NavLink
             to="/about"
+            activeClassName="text-red-100 bg-red-700"
             className="inline-flex items-center py-3 px-3 my-6 rounded text-gray-400 hover:text-gray-100"
           >
             About Me!
           </NavLink>
         </nav>
+        <div></div>
       </div>
     </header>
   );
