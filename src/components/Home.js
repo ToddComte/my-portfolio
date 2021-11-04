2b89069b5c476dab65f895cf1dacb2f1472ff791
ToddComte <comtetodd@gmail.com>
2021-11-04 05:31:59 -0600

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function Home() {
-  return <h1>Home Sweet Home</h1>;
+  return (
+    <main>
+      <img />
+      <section>
+        <h1>my name is Todd</h1>;
+      </section>
+    </main>
+  );
 }
