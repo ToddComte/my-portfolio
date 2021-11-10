398fcf8fc77760e9c81ec4bd501b736244f4f5fb
ToddComte <comtetodd@gmail.com>
2021-11-10 14:48:48 -0700

    troubleshooting sanity

--- src/components/Post.js
+++ src/components/Post.js
@@ -37,7 +37,7 @@ export default function Post() {
           {postData &&
             postData.map((post, index) => (
               <article>
-                <Link to={"/post/" + post.slug.current} key={post.slug.current}>
+                <link to={"/post/" + post.slug.current} key={post.slug.current}>
                   <span
                     className="block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-green-400"
                     key={index}
@@ -53,7 +53,7 @@ export default function Post() {
                       </h3>
                     </span>
                   </span>
-                </Link>
+                </link>
               </article>
             ))}
         </div>
