f03076a6170c440abf6c67ead72ae0b3c5135465
ToddComte <comtetodd@gmail.com>
2021-11-17 09:34:36 -0700

    changing colors for blog post page

--- src/components/Post.js
+++ src/components/Post.js
@@ -25,7 +25,7 @@ export default function Post() {
   }, []);
 
   return (
-    <main className="bg-green-100 min-h-screen p-12">
+    <main className="bg-blue-100 min-h-screen p-12">
       <section className="container mx-auto">
         <h1 className="text-5xl flex justify-center cursive">
           My Blog Posts Page
@@ -39,7 +39,7 @@ export default function Post() {
               <article>
                 <Link to={"/post/" + post.slug.current} key={post.slug.current}>
                   <span
-                    className="block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-green-400"
+                    className="block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-blue-400"
                     key={index}
                   >
                     <img
